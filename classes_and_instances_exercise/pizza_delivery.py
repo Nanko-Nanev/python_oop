cb92b166ea99e6efa44ec02fde524d56af36b133
Nanko Nanev <75886522+Nanko-Nanev@users.noreply.github.com>
2021-02-27 16:59:40 +0200

    Add files via upload

--- classes_and_instances_exercise/pizza_delivery.py
+++ classes_and_instances_exercise/pizza_delivery.py
@@ -10,7 +10,7 @@ class PizzaDelivery:
             return f"Pizza {self.name} already prepared and we can't make any changes!"
         if ingredient not in self.ingredients:
             self.ingredients[ingredient] = 0
-        self.ingredients[ingredient] = quantity
+        self.ingredients[ingredient] += quantity
         self.price += quantity * ingredient_price
 
     def remove_ingredient(self, ingredient: str, quantity: int, ingredient_price: float):
@@ -30,12 +30,12 @@ class PizzaDelivery:
         return f"You've ordered pizza {self.name} prepared with {', '.join(current_ingredients)} and the price will be {self.price}lv."
 
 
-margarita = PizzaDelivery('Margarita', 11, {'cheese': 2, 'tomatoes': 1})
-margarita.add_extra('mozzarella', 1, 0.5)
-margarita.add_extra('cheese', 1, 1)
-margarita.remove_ingredient('cheese', 1, 1)
-print(margarita.remove_ingredient('bacon', 1, 2.5))
-print(margarita.remove_ingredient('tomatoes', 2, 0.5))
-margarita.remove_ingredient('cheese', 2, 1)
-print(margarita.make_order())
-print(margarita.add_extra('cheese', 1, 1))
+# margarita = PizzaDelivery('Margarita', 11, {'cheese': 2, 'tomatoes': 1})
+# margarita.add_extra('mozzarella', 1, 0.5)
+# margarita.add_extra('cheese', 1, 1)
+# margarita.remove_ingredient('cheese', 1, 1)
+# print(margarita.remove_ingredient('bacon', 1, 2.5))
+# print(margarita.remove_ingredient('tomatoes', 2, 0.5))
+# margarita.remove_ingredient('cheese', 2, 1)
+# print(margarita.make_order())
+# print(margarita.add_extra('cheese', 1, 1))
