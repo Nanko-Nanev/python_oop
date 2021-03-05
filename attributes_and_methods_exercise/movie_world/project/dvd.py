8807a828455002c2f38737de6fcb7da271744f26
Nanko Nanev <75886522+Nanko-Nanev@users.noreply.github.com>
2021-03-05 13:18:02 +0200

    Add files via upload

---
+++ attributes_and_methods_exercise/movie_world/project/dvd.py
@@ -0,0 +1,19 @@
+import datetime
+
+class DVD:
+    def __init__(self, name, id, creation_year, creation_month, age_restriction):
+        self.name = name
+        self.id = id
+        self.creation_year = creation_year
+        self.creation_month = creation_month
+        self.age_restriction = age_restriction
+        self.is_rented = False
+
+    def __repr__(self):
+        return f"{self.id}: {self.name} ({self.creation_month} {self.creation_year}) has age restriction {self.age_restriction}. Status: {'rented' if self.is_rented else 'not rented'}"
+
+    @classmethod
+    def from_date(cls, id: int, name: str, date: str, age_restriction: int):
+        month, year = [int(el) for el in date.split(".")[1:]]
+        month = datetime.date(year, month, 1).strftime('%B')
+        return cls(name, id, year, month, age_restriction)  # use __repr__
